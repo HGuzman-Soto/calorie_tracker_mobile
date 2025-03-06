b9bf871bc0f0926816bfced1a75a2f62925303a9
hanselguzman-soto <soto26938@gmail.com>
2025-03-05 19:39:41 -0800

    update

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from routes import calories_routes, food_entries_routes
+from routes import calories_routes, food_entries_routes, auth_routes
 
 from database import db
 
